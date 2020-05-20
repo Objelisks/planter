8fa2ab3544c1c24edd9fe1a23771251d2265a629
Glitch (hello-express) <none>
2020-05-20 20:28:43 +0000

    🌍🛩 Checkpoint ./public/script.js:537574/196

--- public/script.js
+++ public/script.js
@@ -3,9 +3,14 @@
 plant planner
 draw plan of space
 place plants
+pull plant data from api
+auto complete plant names
+plant data table:
+  name, acquired, 
 
 
 stretch goals
 place furniture, rotate objects
+option to identify plants by picture, api
 
 */
