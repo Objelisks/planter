97abd24d41e143ed73bdbb459f562291267afb69
Glitch (hello-express) <none>
2020-05-22 22:40:16 +0000

    🌑🎌 Checkpoint ./package.json:537574/1287 ./public/simplify.js:537574/3195 ./public/script.js:537574/939 ./views/index.html:537574/28

--- public/script.js
+++ public/script.js
@@ -38,37 +38,16 @@ selection
 
 */
 
+import * as simp from './simplify.js'
+console.log(simp)
+
 const makeLine = d3.line()
 
 const state = {
   toolmode: 'plan',
 }
 
-// const walls = []
-
-// const svg = d3.select('.zone')
-//   .append('svg')
-//     .attr("width", 800)
-//     .attr("height", 600)
-
-// svg
-//   .on('mousedown', (data) => {
-//     console.log('wtc', walls)
-//     walls.push([d3.mouse(svg)])
-//     svg.on('mousemove.draw', data => {
-//       walls[walls.length-1].push(d3.mouse(svg))
-//     })
-//   })
-//   .on('mouseup', data => {
-//     svg.on('mousemove.draw', null)
-//   })
-//   .selectAll('.wall').data(walls).join(
-//     enter => enter.append('path').classed('wall', true),
-//     update => data => {console.log(data); update.attr('d', d3.line(data)) },
-//     exit => exit.remove())
-
-
-const walls = [[[10, 10], [10, 20], [20, 10]]]
+const walls = []
 
 const zone = d3.select('.zone')
 const svg = zone.append('svg').attr('width', 800).attr('height', 600)
@@ -80,21 +59,22 @@ const renderWalls = () =>
     .join(
       enter => enter.append('path').classed('wall', true).attr('d', d => makeLine(d)),
       update => update.attr('d', d => makeLine(d)),
-      exit => exit.remove()
-    )
+      exit => exit.remove())
   
-
 zone.on('mousedown', () => {
-  console.log('mousedown', walls)
   walls.push([])
+  
   zone.on('mousemove.draw', () => {
     walls[walls.length-1].push(d3.mouse(zone.node()))
     renderWalls()
   })
+  
   renderWalls()
 })
 .on('mouseup', () => {
   zone.on('mousemove.draw', null)
+  const simplified = simplify(walls[walls.length-1])
+  walls[walls.length-1] = simplified
   renderWalls()
 })
 
